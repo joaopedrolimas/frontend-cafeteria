203cd2c3beeedea86aa7345e2e7d46c575953b23
joão <jpa9450@gmail.com>
2026-09-09 21:15:49 -0300

    inicio do frontend de uma cafeteria

---
+++ cafeteria-frontend/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from './services/api'
+
+// Uma lista reativa de produtos, começando vazia
+const produtos = ref([])
+const carregador = ref(true)
+
+async function buscarProdutos() {
+  carregando.value = true
+}
+
+function incrementar() {
+  contador.value++
+}
+</script>
+
+<template>
+  <h1>Cafeteria! ☕</h1>
+  <p>Cliques: {{contador}}</p>
+  <button @click="incrementar">Clicar</button>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: #8b4513;
+}
+</style>
